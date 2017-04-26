516f20f3d64b8ecd010387cedf5ba8bfc99ab60b
Aaron Lifton <aaronlifton@gmail.com>
2017-04-26 12:35:29 -0700

    updates

--- lib/reviewsio.rb
+++ lib/reviewsio.rb
@@ -12,22 +12,39 @@ module ReviewsIO
       attr_accessor :serializer
     end
 
+    attr_accessor :last_response
+
     API_URL = "https://api.reviews.co.uk"
     base_uri API_URL
+
+    def initialize
+      @default_params = default_params
+    end
 
     def serializer
       self.class.serializer
     end
 
+    def default_params
+      {
+        store: self.class.store,
+        api_key: self.class.api_key
+      }
+    end
+
     def post(path, data)
       self.class.post(
         path,
-        body: data
+        body: @default_params.merge(data)
         )
     end
 
     def insert_review(line_item_params)
-      post("/product/review/new", serializer.new(line_item_params).as_json)
+      @last_response = post("/product/review/new", serializer.new(line_item_params).as_json)
+    end
+
+    def last_response_success?
+      @last_response && @last_response["success"]
     end
   end
 end
